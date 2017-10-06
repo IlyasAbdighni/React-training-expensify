5fb561f660443dcc182a1df4019a17630bc1841b
Ilyas Abdighni <udunig@gmail.com>
2017-10-06 18:11:18 +0200

    testing is added

--- src/tests/actions/expense.test.js
+++ src/tests/actions/expense.test.js
@@ -19,3 +19,35 @@ test('Should setup edit expense action object', () => {
         },
     });
 });
+
+test('Should setup add expense action opject with params', () => {
+    const expenseData = {
+        description: 'This is a test',
+        note: 'Nothing is better for me than writing some codes',
+        amount: 12312,
+        createdAt: 1000,
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String),
+        },
+    });
+});
+
+test('Should setup add expense action opject without params', () => {
+    const expectData = {};
+    const action = addExpense(expectData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            description: '',
+            amount: 0,
+            createdAt: 0,
+            id: expect.any(String),
+            note: '',
+        }
+    });
+});
