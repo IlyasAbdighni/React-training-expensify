7f0a04aa52cd40a5a7eb2d19b079b2c27bb8ae8a
Ilyas Abdighni <udunig@gmail.com>
2017-10-13 14:32:42 +0200

    momnetjs and numeraljs integrated

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
         <Link to={`/edit/${id}`} >
-        <h3>{ description }</h3>
+        <h3>{ numeral(description).format('$0,0.00') }</h3>
         </Link>
-        <p> { amount } - {createdAt} </p>
+        <p> { amount } - {moment(createdAt).format('MMMM Do, YYYY')} </p>
         
     </div>
 );
